a55e77d57b427db7d60a6dbee7b54203ef219a7f
Javi Marin <oscarjavimarin@gmail.com>
2023-07-17 18:09:30 -0500

    añadiendo un archivo

---
+++ projecto git.py
@@ -0,0 +1,10 @@
+git status: este comando se usa para obtener el estado actual del repositorio
+git clone: este comando se usa para fijar objetivos
+git pull: este comando se usa para extraer y descargar contenido desde un repositorio local
+git checkout: este comando se usa para desplazarte entre las ramas creadas por git branch
+git log: este comando se usa para explorar el historial del repositorio
+git branch: este comando se usa para crear, enumerar y eliminar ramas
+git add: este comando se usa para agregar un cambio en el directorio de trabajo en el entorno de ensayo
+git commit: este comando se usa para captura instantanea de los cambios preparados
+git push: este comando se usa para cargar contenido del repositorio local a un repositorio remoto
+git merge: este comando se usa para fusionar uno o mas ramas dentro de la rama que tienes activa
